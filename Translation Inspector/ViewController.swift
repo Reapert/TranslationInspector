ba6e399c8e38d5bbc2c119e22a463713a1e86dab
Soner Yuksel <soner@plasticmobile.com>
2019-07-08 10:44:18 -0400

    Initial Commit

---
+++ Translation Inspector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Translation Inspector
+//
+//  Created by Soner Yuksel on 2019-07-08.
+//  Copyright © 2019 Inspector. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
